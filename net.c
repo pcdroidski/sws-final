278abcd19b27418c41e9f2e9bdf7febe71b1da6d
Jordan Scales <scalesjordan@gmail.com>
2013-11-25 01:05:07 -0500

    Added basic request timeout functionality

--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -21,6 +22,7 @@
 #define MSGBUFSZ 1024
 #define MAX_CONNECTIONS 20
 
+int g_msgsock;
 int sock;
 
 void
@@ -120,6 +122,8 @@ handle_connection(int msgsock)
     int nbytes;
     t_httpreq *req;
     t_httpresp *res;
+    struct sigaction timeout;
+    struct itimerval timer;
 
     if ((pid = fork()) == 0) {
         /* Execute child code */
@@ -143,6 +147,10 @@ handle_connection(int msgsock)
             if (debug)
                 printf("Connection from %s:%d\n", buf, port);
 
+            /* Store the socket fd globally so we can write to it in a
+             * signal handler */
+            g_msgsock = msgsock;
+
             /* Read one line from the client and parse it */
             if ((nbytes = read(msgsock, &msg, MSGBUFSZ-1)) >= 0) {
                 msg[nbytes-1] = '\0';
@@ -158,6 +166,13 @@ handle_connection(int msgsock)
             }
 
             // The stuff in the middle - this is where we check for timeouts
+            memset(&timeout, 0, sizeof (timeout));
+            timeout.sa_handler = &sig_handler;
+            sigaction(SIGALRM, &timeout, NULL);
+
+            timer.it_value.tv_sec = CONNECTION_TIMEOUT_SECONDS;
+            timer.it_value.tv_usec = 0;
+            setitimer(ITIMER_REAL, &timer, NULL);
 
             /* headers! read a line and do nothing with it */
             while (1) {
@@ -282,6 +297,8 @@ void
 sig_handler(int signo)
 {
     pid_t pid;
+    t_httpresp *res;
+
     if (signo == SIGCHLD) {
         pid = wait(0);
         if (debug) {
@@ -292,6 +309,18 @@ sig_handler(int signo)
                 printf("Child process returned error result; %d; %s\n",
                         errno, strerror(errno));
         }
+    } else if (signo == SIGALRM) {
+        res = init_response();
+        if (res == NULL) {
+            fprintf(stderr, "malloc generating response error in timeout");
+            exit(1);
+        }
+
+        res->status = HTTP_TIMEOUT;
+        finalize_response(res);
+        write_response(res, g_msgsock);
+        close(g_msgsock);
+        exit(0);
     } else if (signo == SIGTERM || signo == SIGINT) {
         puts("Shutting down...");
         if (close(sock) != 0)
