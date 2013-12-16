2257f821b76f27ebd2e7c096792a6a0c9d80e425
Patrick Cedrowski <pcedrowski@gmail.com>
2013-12-16 11:06:04 -0500

    Logging final push

--- logging.c
+++ logging.c
@@ -1,14 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "system.h"     
-#include "log.h"
+#include "logging.h"
 
 FILE *file;
 
-void logSetup(char *log_file);
 
-void logMessage (char *message);
+void logSetup(char *log_file){
+    file = fopen(log_file, "a");
 
-void logInfo (char *message, char *log_file);
+    if (file == NULL) {
+        return;
+    }
+}
 
-void logError (char *message, char *log_file);
+void logMessage (char *message)
+{
+    fputs(message, file);
+    fclose(file);
+}
+
+
+void logInfo (char *message, char *log_file)
+{
+    logSetup(log_file);
+    logMessage("[INFO]");
+    logMessage(message);
+    logMessage("\n");
+    
+    if (file)
+        fclose(file);
+}
+
+void logError (char *message, char *log_file)
+{
+    logSetup(log_file);
+    logMessage("[ERROR]");
+    logMessage(message);
+    logMessage("\n");
+    
+    if (file)
+        fclose(file);
+}
--- logging.h
+++ logging.h
@@ -1,44 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "system.h"     
-#include "log.h"
 
 FILE *file;
 
+void logSetup(char *log_file);
 
-void logSetup(char *log_file){
-    file = fopen(log_file, "a");
+void logMessage (char *message);
 
-    if (file == NULL) {
-        return
-    }
-}
+void logInfo (char *message, char *log_file);
 
-void logMessage (char *message)
-{
-    fputs(message, file);
-    fclose(file);
-}
-
-
-void logInfo (char *message, char *log_file)
-{
-    logSetup(log_file);
-    logMessage("[INFO]");
-    logMessage(message);
-    logMessage("\n");
-    
-    if (file)
-        fclose(file);
-}
-
-void logError (char *message, char *log_file)
-{
-    logSetup(log_file);
-    logMessage("[ERROR]");
-    logMessage(message);
-    logMessage("\n");
-    
-    if (file)
-        fclose(file);
-}
+void logError (char *message, char *log_file);
